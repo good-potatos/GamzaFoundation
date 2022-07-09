83d9a14a1bf9c12f7b0611fc1c0a51c1a146c9bb
dev-wimes <dev.wimes@gmail.com>
2022-07-09 19:41:42 +0900

    imp. GamzaFoundation에 dependency를 추가

--- Package.swift
+++ Package.swift
@@ -9,13 +9,57 @@ let package = Package(
     products: [
         .library(
             name: "GamzaFoundation",
-            targets: ["GamzaFoundation"]),
+            targets: ["GamzaFoundation"]
+        ),
+        .library(
+            name: "GamzaFoundation_Dev",
+            targets: ["GamzaFoundation_Dev"]
+        )
     ],
     dependencies: [
+        // MARK: - Product
+        .package(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            .upToNextMajor(from: "0.36.0")
+        ),
+        .package(
+            url: "https://github.com/CombineCommunity/CombineExt.git",
+            from: "1.0.0"
+        ),
+        .package(
+            url: "https://github.com/onevcat/Kingfisher",
+            from: "7.2.0"
+        ),
+        // MARK: - Dev
+        .package(
+            url: "https://github.com/nicklockwood/SwiftFormat",
+            from: "0.49.7"
+        ),
+        .package(
+            url: "https://github.com/FLEXTool/FLEX.git",
+            from: "4.3.0"
+        ),
+        .package(
+            url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git",
+            from: "1.9.0"
+        ),
     ],
     targets: [
         .target(
             name: "GamzaFoundation",
-            dependencies: [])
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "CombineExt", package: "CombineExt"),
+                .product(name: "Kingfisher", package: "Kingfisher"),
+            ]),
+        .target(
+            name: "GamzaFoundation_Dev",
+            dependencies: [
+                .product(name: "FLEX", package: "FLEX"),
+                .product(name: "SwiftyBeaver", package: "SwiftyBeaver"),
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+                .product(name: "CombineExt", package: "CombineExt"),
+                .product(name: "Kingfisher", package: "Kingfisher"),
+            ])
     ]
 )
---
+++ Sources/GamzaFoundation_Dev/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Wimes on 2022/07/09.
+//
+
+import Foundation
